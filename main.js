032967d1b830c4f009ad1ad93f352022cc6cb28e
G-Dev-01 <gotudeko@icloud.com>
2020-10-10 05:35:09 +0000

    update

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('Hello World');
+console.log('hello world')
+let age=30
+
+age=31
+
+
+console.log(age)
+
+
+    
+const person={
+    firtsName:'John',
+    lastName: 'Doe',
+    age:30,
+    hobbies: ['music', 'movies', 'sports'],
+    address: {
+    street: '50 main st',
+    city: 'Boston',
+    state: 'MA'
+    } 
+}
+
+console.log(person);
